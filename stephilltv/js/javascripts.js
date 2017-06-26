f1cffb033168e311be283a3fd95bcc8f9f2a5f65
AndresZapSoto <anfe.mtb@gmail.com>
2017-06-26 17:27:56 -0500

    Add files via upload

---
+++ stephilltv/js/javascripts.js
@@ -0,0 +1,49 @@
+//javascripts
+
+function clickPN(){
+  alert("March 5 - 12 \nFrance");
+}
+/*
+function clickTA(){
+  alert("March 8 - 14 \nItaly");
+}
+
+function clickVC(){
+  alert("March 20 - 26 \nSpain");
+}
+
+function clickPV(){
+  alert("April 3 - 8 \nSpain");
+}
+
+function clickTR(){
+  alert("April 25 - 30 \nSwitzerland");
+}
+
+function clickGI(){
+  alert("May 5 - 28 \nItaly");
+}
+
+function clickCD(){
+  alert("June 4 - 11 \nFrance");
+}
+
+function clickTS(){
+  alert("June 10 - 18 \nSwitzerland");
+}
+
+function clickTF(){
+  alert("July 1 - 23 \nFrance");
+}
+
+function clickTP(){
+  alert("July 12 - 18 \nPologne");
+}
+
+function clickVE(){
+  alert("August 20 - September 11 \nSpain");
+}
+
+function clickET(){
+  alert("September 19 - 25 \nBelgium");
+}*/
